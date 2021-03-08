87d9968d807f1863844bf4d4c733910e60b303dc
saruwka <36380102+saruwka@users.noreply.github.com>
2021-03-08 01:49:52 -0800

    modification on the code

--- script.js
+++ script.js
@@ -37,3 +37,58 @@ const data = {
       },
     ],
   };
+
+  class Cities {
+    constructor(cities) {
+      this.cities = cities;
+      this.slider = document.querySelector(".slider");
+      this.result = document.querySelector(".result");
+      this.time = document.querySelector(".time");
+      this.activeTab;
+      this.timeInterval;
+    }
+    init = () => {
+      // Rendering all cities
+      this.result.innerHTML = this.cities
+        .map((city) => `<li data-zone="${city.timeZone}">${city.label}</li>`)
+        .join("");
+      // Adding an event listener to ul
+      this.result.addEventListener("click", this.onClickCity);
+      // Click first element manually
+      this.result.childNodes[0].click();
+      // Adding resize event listener
+      window.addEventListener("resize", this.moveSlider);
+      // Update time on every second
+      window.setInterval(this.updateTime, 1000);
+    };
+    onClickCity = ({ target }) => {
+      // if clicked element is li and it's not already active li.
+      if (target.nodeName === "LI" && target !== this.activeTab) {
+        // remove previous active class
+        if (this.activeTab) {
+          this.activeTab.classList.remove("active");
+        }
+        // set new active element
+        this.activeTab = target;
+        target.classList.add("active");
+        // move slide
+        this.moveSlider();
+      }
+    };
+    moveSlider = () => {
+      // get offset values from an active element
+      const { offsetWidth, offsetLeft } = this.activeTab;
+      // set width and marginLeft of a slider with activeTab offset values
+      this.slider.style.width = `${offsetWidth}px`;
+      this.slider.style.marginLeft = `${offsetLeft}px`;
+    };
+    updateTime = () => {
+      // Creating time based on time zone
+      const timeInCity = new Date().toLocaleString("en-US", {
+        timeZone: this.activeTab.getAttribute("data-zone"),
+      });
+      this.time.innerHTML = `Current time in ${this.activeTab.innerText} ${timeInCity}`;
+    };
+  }
+  const cities = new Cities(data.cities);
+  cities.init();
