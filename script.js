cfb629a81c52c317794e89ba2e7d9c72bca20149
saruwka <36380102+saruwka@users.noreply.github.com>
2021-03-08 00:45:38 -0800

    comments added

--- script.js
+++ script.js
@@ -38,25 +38,33 @@ const data = {
       this.activeTab;
     }
     init = () => {
+      // Rendering all cities
       this.result.innerHTML = this.cities
         .map((city) => `<li>${city.label}</li>`)
         .join("");
+      // Adding an event listener to ul
       this.result.addEventListener("click", this.onClickCity);
+      // Click first element manually
       this.result.childNodes[0].click();
+      // Adding resize event listener
       window.addEventListener("resize", this.moveSlider);
     };
     onClickCity = ({ target }) => {
+      // if clicked element is li and it's not already active li.
       if (target.nodeName === "LI" && target !== this.activeTab) {
-        if (this.activeTab) {
-          this.activeTab.classList.remove("active");
-        }
+        // remove previous active class
+        this.activeTab.classList.remove("active");
+        // set new active element
         this.activeTab = target;
         target.classList.add("active");
+        // move slide
         this.moveSlider();
       }
     };
     moveSlider = () => {
+      // get offset values from an active element
       const { offsetWidth, offsetLeft } = this.activeTab;
+      // set width and marginLeft of a slider with activeTab offset values
       this.slider.style.width = `${offsetWidth}px`;
       this.slider.style.marginLeft = `${offsetLeft}px`;
     };
